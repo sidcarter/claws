7231a444a043b5d2251790a79d7b35f7a5ae502e
Sid Carter <me@sidcarter.com>
2015-05-25 00:58:54 -0400

    Code to process arguments. lot more work is needed to get this into a good shape.

--- src/claws/core.clj
+++ src/claws/core.clj
@@ -1,28 +1,44 @@
 (ns claws.core
-  (:gen-class claws.core)
-  (:use [amazonica.aws.ec2]))
+  (:require [amazonica.aws.ec2 :as ec2]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class claws.core))
+
+(def cli-options
+  ;; list all the instances or security groups
+  [["-l" "--list" "list either instances or security groups. default is to list instances"
+    :default "instances"]
+  ["-h" "--help" "Usage: claws -l to list all instances"]])
+
+(defn usage
+  [banner]
+  (println banner))
 
 (defn regions
   []
-  ((describe-regions) :regions))
+  ((ec2/describe-regions) :regions))
 
 (defn vpcs
   []
-  ((describe-vpcs) :vpcs))
+  ((ec2/describe-vpcs) :vpcs))
+
+(defn sgs
+  []
+  ((ec2/describe-security-groups) :security-groups))
+
+(defn print_sgs
+  [sgs]
+  (doseq [sg sgs]
+    (println (str (:group-name sg) " - " (:group-id sg)))))
+
+(defn instances
+  []
+  ((ec2/describe-instances) :reservations))
 
 (defn -main
   "I am the main."
   [& args]
-  (if (not= (count args) 0)
-    (println
-      (first args)))
-  (println "Available VPCs: ")
-  (doseq [vpc (vpcs)]
-    (if (= (vpc :state) "available")
-      (doseq [tag (vpc :tags)]
-        (if (= (tag :key) "Name")
-          (println (str (tag :value) "(" (vpc :vpc-id) ") - " (vpc :state)))))))
-  (println "Available Regions: ")
-  (doseq [region (regions)]
-    (println (region :region-name)))
-  )
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    (cond
+      (= (:list options) "instances") (println (instances))
+      (:list options) (println options arguments)
+      (:help options) (println "help"))))
